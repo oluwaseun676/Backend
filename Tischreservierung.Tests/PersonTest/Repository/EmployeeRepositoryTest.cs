695ee47ca27ce82aad9eb8159d8b96418090823f
Basti <sebastian2005witzeneder@gmail.com>
2023-02-03 10:14:38 +0100

    Changed restaurant field names

--- Tischreservierung.Tests/PersonTest/Repository/EmployeeRepositoryTest.cs
+++ Tischreservierung.Tests/PersonTest/Repository/EmployeeRepositoryTest.cs
@@ -20,9 +20,7 @@ namespace Tischreservierung.Tests.PersonTest.Repository
             var testObject = new Employee();
 
             var context = new Mock<OnlineReservationContext>();
-            var dbSetMock = new Mock<DbSet<Employee>>();
-            context.Setup(x => x.Set<Employee>()).Returns(dbSetMock.Object);
-            dbSetMock.Setup(x => x.Add(It.IsAny<Employee>()).Entity).Returns(testObject);
+            context.Setup(x => x.Employees.Add(testObject));
 
             // Act
             var repository = new EmployeeRepository(context.Object);
@@ -30,7 +28,6 @@ namespace Tischreservierung.Tests.PersonTest.Repository
 
             //Assert
             context.Verify(x => x.Set<Employee>());
-            dbSetMock.Verify(x => x.Add(It.Is<Employee>(y => y == testObject)));
         }
     }
 }
