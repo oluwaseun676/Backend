320188be01ceaa216fa5032132b54e1d7e043598
Simon-Raaber <95470326+SimonRaaber@users.noreply.github.com>
2023-03-10 13:33:23 +0100

    sonar qube fixes

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -28,11 +28,9 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
