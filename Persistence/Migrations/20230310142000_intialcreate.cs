9ae3d47da9d1512136e36c952374d16bf9fc1e84
Simon-Raaber <95470326+SimonRaaber@users.noreply.github.com>
2023-03-17 13:20:57 +0100

    added missing dependec y

--- Persistence/Migrations/20230310142000_intialcreate.cs
+++ Persistence/Migrations/20230310142000_intialcreate.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Persistence.Migrations
 {
-    public partial class InitialCreate : Migration
+    public partial class intialcreate : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
