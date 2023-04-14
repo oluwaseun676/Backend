4a2ee2785a84588e82412a3435029dbb1a35ce2a
Simon-Raaber <95470326+SimonRaaber@users.noreply.github.com>
2023-04-14 13:48:57 +0200

    fixed namespaces in tests

--- Tischreservierung.Tests/PersonTest/Repository/EmployeeRepositoryTest.cs
+++ Tischreservierung.Tests/PersonTest/Repository/EmployeeRepositoryTest.cs
@@ -1,16 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Persistence.Data;
-using Persistence.Data.Person;
-using Core.Models.Person;
-using Xunit.Sdk;
-
-namespace Tischreservierung.Tests.PersonTest.Repository
+﻿namespace Tischreservierung.Tests.PersonTest.Repository
 {
     public class EmployeeRepositoryTest
     {
