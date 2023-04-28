4eecb45482f698197a2d9ed2835e6794fdc7244c
Simon-Raaber <95470326+SimonRaaber@users.noreply.github.com>
2023-04-28 14:04:26 +0200

    added description to restaurant

--- Core/DTO/DTO_RestaurantPost.cs
+++ Core/DTO/DTO_RestaurantPost.cs
@@ -11,7 +11,8 @@ namespace Core.DTO
     public class DTO_RestaurantPost
     {
         public int Id { get; set; }
-        public string Name { get; set; } = String.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public ZipCode? ZipCode { get; set; }
         public string Address { get; set; } = string.Empty;
         public string StreetNr { get; set; } = string.Empty;
